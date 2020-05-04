d0894512ee03baef3d12ccad65f5d90c83ed1d08
Dmitry Suvorov <soovorow@gmail.com>
2020-05-04 16:13:01 +0300

    update horn message

--- trackbook/management/commands/daily_report.py
+++ trackbook/management/commands/daily_report.py
@@ -16,13 +16,13 @@ class Command(BaseCommand):
         p = Purchase.objects.filter(is_logged=1, transaction_date__contains=yesterday)
         count = p.count()
 
-        horn = f"📬💌🎉 *Report:* {yesterday} UTC \n"
+        horn = f"📬 💌 🎉 \n*Daily Report:* {yesterday} UTC \n"
 
         if count > 0:
             total = 0
             for purch in p:
                 total += CurrencyConverter(purch.currency, purch.sum).to_usd()
-            horn += "~$%1.2f " % total
+            horn += "Your gain ~$%1.2f " % total
             horn += "(%s) \n" % count
         else:
             horn += "There was no any transactions. Sad, but true."
